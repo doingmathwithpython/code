660b0aee3629f9009c7c7154667721f80bdd7442
Amit Saha <amitsaha.in@gmail.com>
2015-10-18 20:26:10 +1100

    Chapter 5: Solutions, suite should be suit

--- chapter5/solutions/shuffle_enhanced.py
+++ chapter5/solutions/shuffle_enhanced.py
@@ -6,24 +6,24 @@ Shuffle a deck of 52 cards
 import random
 
 class Card:
-    def __init__(self, suite, rank):
-        self.suite = suite
+    def __init__(self, suit, rank):
+        self.suit = suit
         self.rank = rank
         
 def initialize_deck():
-    suites = ['Clubs', 'Diamonds', 'Hearts', 'Spades']
+    suits = ['Clubs', 'Diamonds', 'Hearts', 'Spades']
     ranks = ['Ace', '2', '3','4', '5', '6', '7', '8', '9', '10', 'Jack', 'Queen', 'King']
     cards = []
-    for suite in suites:
+    for suit in suits:
         for rank in ranks:
-            card = Card(suite, rank)
+            card = Card(suit, rank)
             cards.append(card)
     return cards
 
 def shuffle_and_print(cards):
     random.shuffle(cards)
     for card in cards:
-        print('{0} of {1}'.format(card.rank, card.suite))
+        print('{0} of {1}'.format(card.rank, card.suit))
     
 if __name__ == '__main__':
     cards = initialize_deck()
