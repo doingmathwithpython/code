4588c203582a55e021fb54d267097252ec7fea76
Amit Saha <amitsaha.in@gmail.com>
2015-10-19 08:04:05 +1100

    area_curves.py - Rename f1x to f and f2x to g

--- chapter7/solutions/area_curves.py
+++ chapter7/solutions/area_curves.py
@@ -6,22 +6,22 @@ Find the area enclosed by two curves between two points
 
 from sympy import Integral, Symbol, SympifyError, sympify
 
-def find_area(f1x, f2x, var, a, b):
-    a = Integral(f1x-f2x, (var, a, b)).doit()
+def find_area(f, g, var, a, b):
+    a = Integral(f-g, (var, a, b)).doit()
     return a
 
 if __name__ == '__main__':
-    f1x = input('Enter the upper function in one variable: ')
-    f2x = input('Enter the lower upper function in one variable: ')
+    f = input('Enter the upper function in one variable: ')
+    g = input('Enter the lower upper function in one variable: ')
     var = input('Enter the variable: ')
     l = float(input('Enter the lower bound of the enclosed region: '))
     u = float(input('Enter the upper bound of the enclosed region: '))
     
     try:
-        f1x = sympify(f1x)
-        f2x = sympify(f2x)
+        f = sympify(f)
+        g = sympify(g)
     except SympifyError:
         print('One of the functions entered is invalid')
     else:
         var = Symbol(var)
-        print('Area enclosed by {0} and {1} is: {2} '.format(f1x, f2x, find_area(f1x, f2x, var, l, u)))
+        print('Area enclosed by {0} and {1} is: {2} '.format(f, g, find_area(f, g, var, l, u)))
