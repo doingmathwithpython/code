9dff696312a5f026230aa4d84fcb87df656854ea
Amit Saha <amitsaha.in@gmail.com>
2016-03-08 16:53:35 +1100

    Chapter 1: Solutions done

--- chapter1/solutions/run.bash
+++ chapter1/solutions/run.bash
@@ -1,5 +1,12 @@
 #!/bin/bash
 
 for file in `ls *.py`; do
-    echo $file
+    filename=$(basename $file)
+    filename="${filename%.*}"
+    python3 $filename.py < inputs/$filename.input
+
+    if [[ $? != 0 ]]; then
+        echo "Something failed"
+        exit 1
+    fi
 done
