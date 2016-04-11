d45c82121e3d07a2235b166f69404fc50c523b40
Amit Saha <amitsaha@users.noreply.github.com>
2016-04-11 18:17:19 +1000

    Update run.bash

--- run.bash
+++ run.bash
@@ -2,8 +2,11 @@
 
 set -e
 
+echo "Python: " 
 python3 -c "import sys;print(sys.version)"
+echo "Sympy: "
 python3 -c "import sympy;print(sympy.__version__)"
+echo "Matplotlib: "
 python3 -c "import matplotlib; print(matplotlib.__version__)"
 
 for dir in 'chapter1' 'chapter2' 'chapter3' 'chapter4' 'chapter5' 'chapter6' 'chapter7'; do
