019a2d952047624075044dbc4acd2c2607fd5c73
Amit Saha <amitsaha@users.noreply.github.com>
2016-04-11 18:09:19 +1000

    Update run.bash

--- run.bash
+++ run.bash
@@ -2,9 +2,9 @@
 
 set -e
 
-python3 -c "import sys; print(sys.version)" > versions.log
-python3 -c "import sympy; print(sympy.__version__)" >> versions.log
-python3 -c "import matplotlib; print(matplotlib.__version__)" >> versions.log
+python3 -c "import sys;print(sys.version)"
+python3 -c "import sympy;print(sympy.__version__)"
+python3 -c "import matplotlib; print(matplotlib.__version__)"
 
 for dir in 'chapter1' 'chapter2' 'chapter3' 'chapter4' 'chapter5' 'chapter6' 'chapter7'; do
     pushd $dir/solutions
