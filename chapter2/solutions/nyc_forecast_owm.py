63f450363d16920d0028355abb46fd45336656f0
Amit Saha <amitsaha@users.noreply.github.com>
2018-08-10 21:52:19 +1000

    Update nyc_forecast_owm.py

--- chapter2/solutions/nyc_forecast_owm.py
+++ chapter2/solutions/nyc_forecast_owm.py
@@ -1,5 +1,5 @@
 '''
-Retrieve three days' forecast of New York City and create a graph
+Retrieve a day's forecast of New York City and create a graph
 Using https://github.com/csparpa/pyowm
 '''
 import matplotlib.pyplot as plt
@@ -9,16 +9,15 @@ import pytz
 owm = OWM('<token>')
 
 def get_forecast(city):
+    # this used to be daily_forecast
     # https://github.com/csparpa/pyowm/issues/266
     fc = owm.three_hours_forecast(city)
     f = fc.get_forecast()
     
     # three_hours_forecast() returns 5 day forecast at a granularity
     # of three hours
-    # we plot them all
-    weathers = f.get_weathers()
-    
-    
+    # We plot the forecast for the next 24 hours (so, first 8 intervals)
+    weathers = f.get_weathers()[0:8]
     data_points = ['temp', 'temp_max', 'temp_min', 'temp_kf']
     temp = []
     date_time = []
@@ -29,12 +28,15 @@ def get_forecast(city):
         dt = utc_dt.astimezone(tz)
 
         # print it
-        date_time.append(dt.strftime('%Y-%m-%d %H:%M:%S %Z%z'))
+        date_time.append(dt.strftime('%Y-%m-%d %H:%M'))
         temp.append(forecast_temp['temp'])
     x = range(1, len(temp)+1)
     plt.plot(x, temp, 'o-')
-    plt.xticks(x, date_time)
+    plt.xticks(x, date_time, rotation=45)
     plt.show()
+
+if __name__ == '__main__':
+    get_forecast('new york, us')
 
 if __name__ == '__main__':
     get_forecast('new york, us')
